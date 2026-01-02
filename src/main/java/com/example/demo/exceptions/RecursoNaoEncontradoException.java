8e504c5ff434d9ba02593345795c08ab152148f3
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 16:13:56 -0400

    feat: adiciona exceção de produto não encontrado

---
+++ src/main/java/com/example/demo/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,10 @@
+package com.example.demo.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException{
+
+    public RecursoNaoEncontradoException(String mensagem){
+        super(mensagem);
+    }
+
+
+}
