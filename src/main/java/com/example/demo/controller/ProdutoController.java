c873e4b52cbc18f0daf1c7919aa85483982ba0e8
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 16:13:18 -0400

    fix; readiciona controller

---
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exceptions.RecursoNaoEncontradoException;
+import com.example.demo.model.Produto;
+import com.example.demo.service.ProdutoService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtoService.listarProdutos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarProduto(@PathVariable Long id) {
+        try{
+        Produto produto = produtoService.buscarPorId(id);
+        return ResponseEntity.ok(produto);
+        }
+        catch(RecursoNaoEncontradoException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public Produto criarProduto(@RequestBody Produto produto) {
+        return produtoService.salvarProduto(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        produtoService.deletarProduto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
