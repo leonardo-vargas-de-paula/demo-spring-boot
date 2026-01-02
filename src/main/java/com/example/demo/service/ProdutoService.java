4fce0e7b7cf94a1799b41e2b5e37322ac7eaf017
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-02 16:13:41 -0400

    feat: adiciona exceções de not found

--- src/main/java/com/example/demo/service/ProdutoService.java
+++ src/main/java/com/example/demo/service/ProdutoService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.exceptions.RecursoNaoEncontradoException;
 import com.example.demo.model.Produto;
 import com.example.demo.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -18,5 +20,24 @@ public class ProdutoService {
     public List<Produto> listarProdutos(){
         return produtoRepository.findAll();
     }
+
+    public Produto buscarPorId(Long id){
+        return produtoRepository.findById(id)
+                .orElseThrow(()-> new RecursoNaoEncontradoException("Produto não encontrado"));
+    }
+
+    public Produto salvarProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public void deletarProduto(Long id){
+
+        if (!produtoRepository.existsById(id)){
+            throw new RecursoNaoEncontradoException("Produto não encontrado");
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
 }
 
