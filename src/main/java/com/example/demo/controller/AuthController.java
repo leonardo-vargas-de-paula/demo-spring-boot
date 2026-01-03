d3c3e22d64b2e7116a04f15b708b98699eafd9fb
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-03 09:39:31 -0400

    feat: add endpoint de login

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Usuario;
+import com.example.demo.security.JwtUtil;
 import com.example.demo.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,9 +25,20 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Map<String, String> request){
         Usuario usuario = usuarioService.registrarUsuario(request.get("username"), "password");
-
         return ResponseEntity.ok(usuario);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request){
+        Optional<Usuario> usuario = usuarioService.buscarPorUsername(request.get("username"));
+        if (usuario.isPresent() && usuario.get().getPassword().equals(request.get("password"))){
+            String token = JwtUtil.generateToken(usuario.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+
+        return ResponseEntity.status(401).body("Credenciais inválidas");
+    }
+
 
 
 
