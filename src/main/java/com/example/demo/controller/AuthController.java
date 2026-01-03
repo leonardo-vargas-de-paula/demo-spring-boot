78d9ef701a5c4771bb144bb35f730fd6508cab72
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-03 09:31:06 -0400

    feat: add auth controller e endpoint de register

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UsuarioService usuarioService;
+
+    public AuthController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request){
+        Usuario usuario = usuarioService.registrarUsuario(request.get("username"), "password");
+
+        return ResponseEntity.ok(usuario);
+    }
+
+
+
+}
