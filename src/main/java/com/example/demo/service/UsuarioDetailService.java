bfd95c22964614bf6acee7804f776cf957e1c184
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-03 11:41:37 -0400

    feat: add usuariodetailservice

---
+++ src/main/java/com/example/demo/service/UsuarioDetailService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioDetailService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioDetailService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
